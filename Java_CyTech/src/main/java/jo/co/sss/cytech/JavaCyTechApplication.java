5fd9508e35be9e3bc9c6a0163f3f09e3b27eb0e1
nannanwww <engineerookie@gmail.com>
2024-06-16 14:12:57 +0900

    initial Commit

---
+++ Java_CyTech/src/main/java/jo/co/sss/cytech/JavaCyTechApplication.java
@@ -0,0 +1,13 @@
+package jo.co.sss.cytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCyTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCyTechApplication.class, args);
+	}
+
+}
---
+++ Java_CyTech/src/test/java/jo/co/sss/cytech/JavaCyTechApplicationTests.java
@@ -0,0 +1,13 @@
+package jo.co.sss.cytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCyTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
